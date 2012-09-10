87a4b844cf01af184db89e9a39edebd411818088
targoo <targoo@gmail.com>
2012-09-10 22:34:30 +0100

    add default value in settings

--- color_field.jquery.js
+++ color_field.jquery.js
@@ -6,8 +6,10 @@
   Drupal.behaviors.color_field = {
     attach: function (context) {
       $.each(Drupal.settings.color_field, function (selector) {
+        console.log(this);
         $(selector).empty().addColorPicker({
           currentColor:this.value,
+          colors:this.colors,
           clickCallback: function(c) {
             id = selector;
             id = id.replace("#div","edit");
