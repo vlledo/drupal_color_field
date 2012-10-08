16148b72d6ac0679d8d748d9816cb6ca7e7453f4
targoo <targoo@gmail.com>
2012-10-08 21:47:07 +0100

    clean code

--- color_field_jquery_simple_color/color_field_jquery_simple_color.jquery.js
+++ color_field_jquery_simple_color/color_field_jquery_simple_color.jquery.js
@@ -8,7 +8,7 @@
       $.each(Drupal.settings.color_field_jquery_simple_color, function (selector) {
         $(selector).empty().simpleColor({
           cellWidth: this.cell_width,
-    	  cellHeight: this.cell_height,
+          cellHeight: this.cell_height,
           cellMargin: this.cell_margin,
           boxWidth: this.box_height,
           boxHeight: this.box_width,
