8d46988b46ca27543dd46d9037f7f01158ee8a38
targoo <targoo@gmail.com>
2012-10-08 21:32:49 +0100

    add jquery-simple-color support

---
+++ color_field_jquery_simple_color/color_field_jquery_simple_color.jquery.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * Javascript for Color Field.
+ */
+(function ($) {
+  Drupal.behaviors.color_field_jquery_simple_color = {
+    attach: function (context) {
+      $.each(Drupal.settings.color_field_jquery_simple_color, function (selector) {
+        $(selector).empty().simpleColor({
+          cellWidth: this.cell_width,
+    	  cellHeight: this.cell_height,
+          cellMargin: this.cell_margin,
+          boxWidth: this.box_height,
+          boxHeight: this.box_width,
+          columns: this.columns
+        });
+      });
+    }
+  };
+})(jQuery);
