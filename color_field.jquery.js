d2c44d95bd2dbce0c4153bde4af0d7cdbb6c5114
targoo <targoo@gmail.com>
2012-10-18 14:18:01 +0100

    remove console log

--- color_field.jquery.js
+++ color_field.jquery.js
@@ -6,7 +6,6 @@
   Drupal.behaviors.color_field = {
     attach: function (context) {
       $.each(Drupal.settings.color_field, function (selector) {
-        console.log(this);
         $(selector).empty().addColorPicker({
           currentColor:this.value,
           colors:this.colors,
