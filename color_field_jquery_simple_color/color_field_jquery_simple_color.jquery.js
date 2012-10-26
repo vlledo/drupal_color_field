370d482acb656cc82724bb6a4e2d5e0f8c5bfbed
targoo <targoo@gmail.com>
2012-10-26 10:20:59 +0100

    bug with multiple value

--- color_field_jquery_simple_color/color_field_jquery_simple_color.jquery.js
+++ color_field_jquery_simple_color/color_field_jquery_simple_color.jquery.js
@@ -6,13 +6,16 @@
   Drupal.behaviors.color_field_jquery_simple_color = {
     attach: function (context) {
       $.each(Drupal.settings.color_field_jquery_simple_color, function (selector) {
-        $(selector).empty().simpleColor({
-          cellWidth: this.cell_width,
-          cellHeight: this.cell_height,
-          cellMargin: this.cell_margin,
-          boxWidth: this.box_width,
-          boxHeight: this.box_height
-        });
+    	if (!$(selector).hasClass('jquery-simple-color-init')) {
+          $(selector).simpleColor({
+            cellWidth: this.cell_width,
+            cellHeight: this.cell_height,
+            cellMargin: this.cell_margin,
+            boxWidth: this.box_width,
+            boxHeight: this.box_height
+          });
+    	  $(selector).addClass('jquery-simple-color-init');
+        }
       });
     }
   };
