d017067085d9a38f85f47b9d75e1f4a4fa4c491e
targoo <targoo@gmail.com>
2012-10-26 10:26:54 +0100

    coding standard

--- color_field_jquery_simple_color/color_field_jquery_simple_color.jquery.js
+++ color_field_jquery_simple_color/color_field_jquery_simple_color.jquery.js
@@ -6,7 +6,7 @@
   Drupal.behaviors.color_field_jquery_simple_color = {
     attach: function (context) {
       $.each(Drupal.settings.color_field_jquery_simple_color, function (selector) {
-    	if (!$(selector).hasClass('jquery-simple-color-init')) {
+        if (!$(selector).hasClass('jquery-simple-color-init')) {
           $(selector).simpleColor({
             cellWidth: this.cell_width,
             cellHeight: this.cell_height,
@@ -14,7 +14,7 @@
             boxWidth: this.box_width,
             boxHeight: this.box_height
           });
-    	  $(selector).addClass('jquery-simple-color-init');
+          $(selector).addClass('jquery-simple-color-init');
         }
       });
     }
