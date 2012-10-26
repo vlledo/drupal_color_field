f478258bd0c2b8c63a4a59dd9cbce5887d3c0d36
targoo <targoo@gmail.com>
2012-10-26 10:30:15 +0100

    coding standard

--- color_field_farbtastic/color_field_farbtastic.jquery.js
+++ color_field_farbtastic/color_field_farbtastic.jquery.js
@@ -6,11 +6,8 @@
   Drupal.behaviors.color_field_farbtastic = {
     attach: function (context) {
 	  $.each(Drupal.settings.color_field_farbtastic, function (selector) {
-        console.log(selector);
-        console.log(this.id);
-        console.log(this.picker_id);
         $('#' + this.picker_id).farbtastic('#' + this.id);
-	  });
+      });
     }
   };
 })(jQuery);
