0b2013a787605cca960e3f5c2a40516dff3b933a
targoo <targoo@gmail.com>
2012-10-26 10:32:19 +0100

    color_field_farbtastic_widget need more work before release

--- color_field_farbtastic/color_field_farbtastic.jquery.js
+++ color_field_farbtastic/color_field_farbtastic.jquery.js
@@ -5,7 +5,7 @@
 (function ($) {
   Drupal.behaviors.color_field_farbtastic = {
     attach: function (context) {
-	  $.each(Drupal.settings.color_field_farbtastic, function (selector) {
+      $.each(Drupal.settings.color_field_farbtastic, function (selector) {
         $('#' + this.picker_id).farbtastic('#' + this.id);
       });
     }
