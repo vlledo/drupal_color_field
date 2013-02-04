736c05a1edd0d47b8f52d6c849a9c3a5184733c1
targoo <targoo@gmail.com>
2013-02-04 09:51:23 +0000

    reorganise js and css for default widget

---
+++ color_field_default/color_field.jquery.js
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * Javascript for Color Field.
+ */
+(function ($) {
+  Drupal.behaviors.color_field = {
+    attach: function (context) {
+      $.each(Drupal.settings.color_field, function (selector) {
+    	id = selector;
+        id = id.replace("#div","edit");
+        value = $('#' + id).val();
+        if (value == '') value = this.value;
+        $(selector).empty().addColorPicker({
+          currentColor:value,
+          colors:this.colors,
+          clickCallback: function(c) {
+            id = selector;
+            id = id.replace("#div","edit");
+            $('#' + id).val(c);
+          }
+        });
+      });
+    }
+  };
+})(jQuery);
