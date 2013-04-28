789588c83371e8fc0655bf8a4c31344aeca7e515
targoo <targoo@gmail.com>
2013-04-28 21:36:26 +0100

    add more option for spectrum color picker

--- color_field_spectrum_color_picker/color_field_spectrum_color_picker.jquery.js
+++ color_field_spectrum_color_picker/color_field_spectrum_color_picker.jquery.js
@@ -6,16 +6,16 @@
   Drupal.behaviors.color_field_spectrum = {
     attach: function (context) {
       $.each(Drupal.settings.color_field_spectrum, function (selector) {
+        console.log(this);
         $('#' + this.id).spectrum({
           preferredFormat: "hex",
           showInput: this.show_input,
           showAlpha: this.show_alpha,
-          /*className: this.class_name,
-          showButtons: this.show_buttons,
-          chooseText: this.choose_text,
-          cancelText: this.cancel_text,
+          showInitial: true,
           showPalette: this.show_palette,
-          palette: [this.palette]*/
+          showPaletteOnly: this.show_palette_only,
+          palette: [this.show_palette],
+          showButtons: this.show_buttons,
         });
       });
     }
